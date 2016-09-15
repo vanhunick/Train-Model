6a7a15d0357b443270b6abd7e5cbda55d8a55720
vanhunick <nickyvanhulst@gmail.com>
2016-09-15 19:05:30 +1200

    Commenting trains package spec

--- src/trains.ads
+++ src/trains.ads
@@ -1,8 +1,6 @@
 package Trains with SPARK_Mode => on is
-
    -- Type that holds the pump data
    --     type Train is private;
-
    -- Public for proving
    type Train is
       record
@@ -11,33 +9,28 @@ package Trains with SPARK_Mode => on is
          Cur_Location_ID : Natural;
       end record;
 
-
    -- Creates an instance of the pump type and returns it
    function Create(ID : in Natural; Start_Location_ID : in Natural)return Train with
      Post => Get_ID(Create'Result) = ID and then
      Get_Location(Create'Result) = Start_Location_ID;
 
-   -- ===========================================================
-   --                 Update Procedures
-   -- ===========================================================
-
+   -- Sets the current location of the train to be the destination
    procedure Update_Location(A_Train : in out Train) with
-     Post => A_Train.Cur_Location_ID = A_Train.Destination;
+     Post => A_Train.Cur_Location_ID = A_Train'old.Destination;
 
-
+   -- A setter method for the destination of the train
    procedure Set_Destination(A_Train : in out Train; ID : in Natural) with
      post => A_Train.Destination = ID;
 
-   -- ===========================================================
-   --                 Get Functions
-   -- ===========================================================
-
+   -- Returns the ID of the trains
    function Get_ID(A_Train : in Train)return Natural with
      post => Get_ID'Result = A_Train.ID;
 
+   -- Returns the Current location of the train
    function Get_Location(A_Train : in Train)return Natural with
      Post => Get_Location'Result = A_Train.Cur_Location_ID;
 
+   -- Returns the destination of the train
    function Get_Destination(A_Train : in Train)return Natural with
      Post => Get_Destination'Result = A_Train.Destination;
 
